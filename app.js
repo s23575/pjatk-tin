55a8420de49aeb0042610df1f3725c6f92ded487
s23575 <s23575@pjwstk.edu.pl>
2021-12-24 00:17:51 +0100

    Projekt semestralny

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
+function getNodeName(name) {
+  $.ajax({
+      type: "get",
+      url: "https://www.gov.pl/attachment/df9f95d2-ed06-4df2-9338-b486174f124c",
+      dataType: "xml",
+      success: function(data) {
+          /* handle data here */
+          console.log($.isXMLDoc(data));
+          $x = $(data);
+          // $a = new XMLSerializer().serializeToString(data);
+          // console.log($a);
+          $y = $x.find("xsd\\:element[name='" + name + "'] > xsd\\:annotation > xsd\\:documentation:first");
+          console.log($y.text());
+      },
+      error: function(xhr, status) {
+          /* handle error here */
+          console.log("nie ok");
+      }
+  });
+}
+
 function previewFile() {
-  const content = document.querySelector('.content');
-  const [file] = document.querySelector('input[type=file]').files;
-  const reader = new FileReader();
+  var [file] = document.querySelector('input[type=file]').files;
+  var reader = new FileReader();
 
   reader.addEventListener("load", () => {
 
-//    const parser = new DOMParser();
     var xmlString = reader.result;
     var xml = $.parseXML(xmlString);
     $xml = $(xml);
+    console.log($.isXMLDoc(xml));
     $adres = $xml.find('jin\\:Aktywa');
-    // console.log($adres.text());
 
-logChildren($adres);
+    logChildren($adres);
 
-function logChildren( $parent ) {
-    $parent.children().each( function( i, child ) {
-        if ($(this).children().length > 1) {
-          console.log( child.nodeName.replace(/([a-z])([A-Z])/g, '$1 $2') + " : ");
-          logChildren( $(child) );
-        } else {
-          console.log( child.nodeName.replace(/([a-z])([A-Z])/g, '$1 $2') + " : " + child.textContent);
-          logChildren( $(child) );
-        }
-    });
-}
-
-    // $adres.each(function( index ) {
-    //   console.log( index + ": " + $( this ).text() );
-    // });
-
-
-
-    // const doc1 = parser.parseFromString(xmlString, "application/xml");
-    // content.innerText = doc1.getElementsByTagName("dtsf:OkresOd")[0].childNodes[0].nodeValue;
-    //
-    // const elements = doc1.querySelectorAll("Naglowek")[0].childNodes;
+    function logChildren( $parent ) {
+      $parent.children().each( function( i, child ) {
+          if ($(this).children().length > 1) {
+            console.log( child.nodeName.replace(/([a-z])([A-Z])/g, '$1 $2') + " : ");
+            logChildren( $(child) );
+          } else {
+            console.log( child.nodeName.replace(/([a-z])([A-Z])/g, '$1 $2') + " : " + child.textContent);
+            logChildren( $(child) );
+          }
+      });
+    }
 
   }, false);
 
