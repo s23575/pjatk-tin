36da4a6b0e5e3590d0213cde8728fa02650fb059
s23575 <s23575@pjwstk.edu.pl>
2021-12-24 15:09:41 +0100

    Projekt semestralny

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var $x;
+
 $.ajax({
     type: "get",
     url: "https://www.gov.pl/attachment/df9f95d2-ed06-4df2-9338-b486174f124c",
@@ -10,11 +12,26 @@ $.ajax({
         /* handle error here */
         console.log("nie ok");
     }
+
 });
 
 function getNodeName(name) {
   $y = $x.find("xsd\\:element[name='" + name + "'] > xsd\\:annotation > xsd\\:documentation:first");
   console.log($y.text());
+}
+
+function logChildren( $parent ) {
+  var s;
+  $parent.children().each( function( i, child ) {
+      if ($(this).children().length > 1) {
+        s = child.nodeName.substring(child.nodeName.indexOf(":") + 1);
+        getNodeName(s);
+        logChildren( $(child) );
+      } else {
+        console.log( child.nodeName.replace(/([a-z])([A-Z])/g, '$1 $2') + " : " + child.textContent);
+        logChildren( $(child) );
+      }
+  });
 }
 
 function previewFile() {
@@ -30,20 +47,6 @@ function previewFile() {
     $adres = $xml.find('jin\\:Aktywa');
 
     logChildren($adres);
-
-    function logChildren( $parent ) {
-      var s;
-      $parent.children().each( function( i, child ) {
-          if ($(this).children().length > 1) {
-            s = child.nodeName.substring(child.nodeName.indexOf(":") + 1);
-            getNodeName(s);
-            logChildren( $(child) );
-          } else {
-            console.log( child.nodeName.replace(/([a-z])([A-Z])/g, '$1 $2') + " : " + child.textContent);
-            logChildren( $(child) );
-          }
-      });
-    }
 
   }, false);
 
